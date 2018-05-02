120f28552ce29923f78f539ad1afd0fab14660e2
Uskuki <uk_uskuki@yahoo.com>
2018-05-02 21:06:11 +0400

    Test1

--- Inucy_test_web/Inucy_test_web/Models/Computerpost.cs
+++ Inucy_test_web/Inucy_test_web/Models/Computerpost.cs
@@ -2,11 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Newtonsoft.Json;
 
 namespace Inucy_test_web.Models
 {
     public class ComputerPost
     {
+        public int IdComputer { get; set;}
+        public char Computer { get; set; }
+        public char Type { get; set; }
+        public char Status { get; set; }
+        public char Os { get; set; }
+        public char Workplace { get; set; }
+        public char Location { get; set; }
 
+        [JsonIgnore]
+        public AppDb Db { get; set; }
+
+        public ComputerPost(AppDb db = null)
+        {
+            Db = db;
+        }
+        public 
     }
 }
